a7baac7e6334b3f72937802edbdf405a6d772063
wcabrera-2022060 <wcabrera-2022060@kinal.edu.gt>
2024-06-04 04:07:48 -0600

    Implementacion de mongo db atlas

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export const connectdb = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/myapp')
+    await mongoose.connect('mongodb+srv://des:41404128Cel@cluster0.aav35uw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     console.log('Database connected')
   } catch (error) {
     console.log('Error connecting to database', error)
