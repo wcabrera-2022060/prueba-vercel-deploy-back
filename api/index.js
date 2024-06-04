7f80f7065ee10c94612fbddf1c4414270c35cf38
wcabrera-2022060 <wcabrera-2022060@kinal.edu.gt>
2024-06-04 01:41:42 -0600

    test3

---
+++ api/index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  const htmlResponse = `
+<html>
+<head>
+<title>NodeJs y Express en Vercel</title>
+</head>
+< body>
+<h1>Soy un proyecto back end
+en vercel</h1>
+</body>
+</html>`
+  res.send(htmlResponse)
+})
+
+app.get('/api', (req, res) => {
+  res.send('Hello from API')
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
