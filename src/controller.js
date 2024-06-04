3415652b038b1b1348c81312c8df55f67ae71b4d
wcabrera-2022060 <wcabrera-2022060@kinal.edu.gt>
2024-06-04 00:47:26 -0600

    ruta por defecto

--- src/controller.js
+++ src/controller.js
@@ -4,7 +4,7 @@ export const create = async (req, res) => {
   try {
     const user = new User(req.body)
     await user.save()
-    res.status(201).send({message: 'User created', user})
+    res.status(201).send({ message: 'User created', user })
   } catch (error) {
     res.status(500).send({ message: error.message })
   }
@@ -12,4 +12,19 @@ export const create = async (req, res) => {
 
 export const test = (req, res) => {
   res.send({ message: 'Hello World' })
+}
+
+export const list = async (req, res) => {
+  const htmlResponse =
+    `<html>
+      <head>
+        <title>NodeJs y Express en Vercel</title>
+      </head>
+      <body>
+        <h1>Soy un proyecto back end
+          en vercel</h1>
+      </body>
+    </html>`
+
+    res.send(htmlResponse)
 }
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { create, test } from './controller.js'
+import { create, list, test } from './controller.js'
 
 const api = Router()
 
 api.post('/create', create)
 api.get('/test', test)
+api.get('/', list)
 
 export default api
