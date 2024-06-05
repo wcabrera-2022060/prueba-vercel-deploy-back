c9dcf9b700603ac4f9138ab33e82317a49db66ec
wcabrera-2022060 <wcabrera-2022060@kinal.edu.gt>
2024-06-05 02:19:28 -0600

    añadiendo y configurando loggers y cors

--- config/app.js
+++ config/app.js
@@ -1,4 +1,7 @@
+import cors from 'cors'
 import express from 'express'
+import helmet from 'helmet'
+import morgan from 'morgan'
 import routes from '../src/routes.js'
 
 const app = express()
@@ -7,6 +10,9 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cors())
+app.use(morgan('dev'))
+app.use(helmet())
 
 app.use(routes)
 
